4a93c082b51ceedbb0bc4225c8e25541afd337d1
Tomatkungen <mr_a2@hotmail.com>
2023-04-04 22:04:04 +0200

    chore: remove readme

--- parse.js
+++ parse.js
@@ -1,50 +1,66 @@
 const util = require('node:util');
 const exec = util.promisify(require('node:child_process').exec);
-import { argv } from 'node:process';
+const { argv } = require('node:process');
 
-// Const
-const git_log_author = 'git log -i --author="\\(tomat\\)" --pretty=format:"{%n \\"author\\":{ \\"date\\": \\"%ai\\", \\"name\\": \\"%an\\", \\"email\\": \\"%ae\\" } %n}" --name-status';
-const git_argv = get_arg();
 
-const git_show_author = async () => {
-    const { stdout, stderr, error } = await exec(git_log_author);
+// Commands
+const git_show_json = async () => {
+    const git_log_users = get_arg().users.map((u) => (`\\(${u}\\)`)).join('\\|');
+    const command_git_log_author = `git log -i --author="${git_log_users}" --pretty=format:"{%n \\"author\\":{ \\"date\\": \\"%ai\\", \\"name\\": \\"%an\\", \\"email\\": \\"%ae\\" } %n}" --name-status`;
+    const command_cd = `cd ${get_arg().path}`;
 
-    if (error) {
-        console.error(`exec error: ${error}`);
-        return null;
-    }
+    const { stdout, stderr, error } = await exec(`${command_cd} && ${command_git_log_author}`);
 
-    if (stderr) {
-        console.error(`exec stderr: ${stderr}`);
-        return null;
-    }
+    if (has_error(error, stderr))
+        return;
 
     return create_show_author_json(stdout);
 }
 
+const has_error = (error, stderr) => {
+    if (error) { console.error(`exec error: ${error}`); return true; }
+    if (stderr) { console.error(`exec stderr: ${stderr}`); return true; }
+
+    return false;
+}
+
 const create_show_author_json = (gitLog) => {
-    if (!gitLog) {
-        return "";
-    }
+    if (!gitLog) return "";
 
-    const lines = gitlog.split('\n');
+    const lines = gitLog.split('\n');
+    /** Example
+        {
+            "author":{ "date": "2023-04-04 21:18:03 +0200", "name": "Tomatkungen", "email": "mr_a2@hotmail.com" }
+        }
+        M	parse.js
+     */
 
-    console.log(lines);
+    console.log(gitLog);
+    lines.forEach((line) => {
+
+    });
 }
 
 const get_arg = () => {
     const arg = { // Default
         path: '.',
+        users: ["tomat"]
     };
+
     argv.forEach((val, index) => {
         console.log(`${index}: ${val}`);
 
         if (val.startsWith('--path=')) {
             arg.path = val.split('=')[1];
+        } 
+        if (val.startsWith('--users=')) {
+            arg.users = val.split('=')[0].split(',');
         }
     });
 
     return arg;
 }
 
-git_show_author();
+(async () => {
+    await git_show_json();
+})()
